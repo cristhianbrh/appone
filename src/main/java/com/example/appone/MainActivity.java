ba37b4f87f6017c98f55c880aacdd18b5b311ed3
cristhianbrh <cristhianalexand@gmail.com>
2025-04-01 20:21:31 -0500

    d

--- src/main/java/com/example/appone/MainActivity.java
+++ src/main/java/com/example/appone/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    //lol, nuevo cambio dsdss
+    /// fdsfsdfdsfdsfsf
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
